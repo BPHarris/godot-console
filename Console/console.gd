ac0c93d6476e8f3182320a62abb7f96fba80cab4
bpharris <brandon.harris1@btinternet.com>
2020-02-13 17:52:14 +0000

    Added command mini language grammar to comments

--- Console/console.gd
+++ Console/console.gd
@@ -1,5 +1,19 @@
 """Godot Console
 
+Console Command Mini Language:
+	command         ::= [a-zA-Z_]+
+	
+	arguments       ::= (' ' argument)+
+	argument        ::= 'true'          ; bool
+					|	'false'         ; bool
+					|	[0-9]+          ; int
+					|	[0-9]+.[0-9]+   ; float
+					|	[a-zA-Z]+       ; small string/name
+					|	'"' [c]+ '"'    ; long string, for any character c
+					|	'$' [a-zA-Z.]+  ; node reference
+	
+	command_line    ::= command arguments?
+
 Usage:
 	- 
 
